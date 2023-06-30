57a901cb07ba711320d101a02250ab20d3d3bfba
BuildTools <unconfigured@null.spigotmc.org>
2023-06-30 10:07:45 +0200

    importing old code

---
+++ jarvice_cli/__main__.py
@@ -0,0 +1,366 @@
+import configparser
+from pathlib import Path
+import sys
+import typer
+import os
+from typing import Optional, Annotated, Dict
+
+#from RESTInterface import *
+from jarviceapi import *
+
+jarvice_cli = typer.Typer(help="The JARVICE CLI for interacting with Jarvice XE")
+
+api_values : Dict[str, str]= {}
+
+def validateJobidJobname(jobid: Optional[int], jobname: Optional[str]):
+    if jobid is None and jobname is None:
+        raise typer.BadParameter("Define jobid or jobname")
+    if jobid is not None and jobname is not None:
+        raise typer.BadParameter("jobid and jobname both specified. Specify only one")
+
+def getEnvValue(argName: str, argValue: Optional[str], envVarName: str):
+    """
+    If parameter is None, it is not in command.
+    First it searches in env variables, second, it searches in ~/.jarvice.cfg
+    """
+    if argValue is None:
+        if envVarName in os.environ:
+            api_values[argName] = os.environ[envVarName]
+        elif os.path.exists(os.path.expanduser("~/.jarvice.cfg")):
+            CParser = configparser.ConfigParser()
+            CParser.read(
+                [
+                    os.path.expanduser("~/.jarvice.cfg"),
+                ]
+            )
+            try:
+                api_values[argName] = CParser.get("auth", argName)
+            except:
+                pass
+        else:
+            
+            raise typer.BadParameter(f"{argName} undefined\n"
+                "username, apikey and url must be passed as arguments, "
+                "or in env variables (JARVICE_USER, JARVICE_API_KEY, JARVICE_API_URL) "
+                "or set in ~/.jarvice.cfg")
+    else:
+        api_values[argName] = argValue
+
+
+@jarvice_cli.callback()
+def getCredentials(
+    ctx: typer.Context,
+    url: Annotated[
+        Optional[str], typer.Option("--url", help="URL of Jarvice XE")
+    ] = None,
+    username: Annotated[
+        Optional[str], typer.Option("--username", "-u", help="Login name in Jarvice XE")
+    ] = None,
+    apikey: Annotated[
+        Optional[str], typer.Option("--apikey", "-k", help="API key in Jarvice XE")
+    ] = None,
+):
+
+    if not any([x in sys.argv for x in ctx.help_option_names]):
+        getEnvValue("username", username, "JARVICE_USER")
+        getEnvValue("apikey", apikey, "JARVICE_API_KEY")
+        getEnvValue("url", url, "JARVICE_API_URL")
+        global jarvice_api
+        jarvice_api = jarviceapi(api_values["username"], api_values["apikey"], api_values["url"])
+
+## Interacting with jobs ##
+
+@jarvice_cli.command()
+def submit(
+    job_json: Annotated[
+        Path,
+        typer.Argument(
+            help="json file with job submission",
+            exists=True,
+            readable=True,
+            dir_okay=False,
+            file_okay=True,
+            resolve_path=True,
+        ),
+    ],
+):
+    """
+    Submit a job
+    """
+    try:
+        jarvice_api.submit(job_json)
+    except Exception as e:
+        print(e)
+
+@jarvice_cli.command()
+def summary():
+    """
+    List a summary of your jobs
+    """
+    try:
+        jarvice_api.summary()
+    except Exception as e:
+        print(e)
+
+
+@jarvice_cli.command()
+def tail(
+    jobid: Annotated[Optional[int], typer.Option("-j", "--jobid",help="ID of the job [required or --jobname required]")] = None,
+    jobname: Annotated[Optional[str], typer.Option("-n", "--jobname",help="Name of the job [required or --jobid required]")] = None,
+    lines: Annotated[
+        Optional[int], typer.Option("-l", "--lines",  help="Number of lines to display")
+    ] = 0
+):
+    """
+    See the output/error of a currently running job
+    """
+    validateJobidJobname(jobid, jobname)
+    
+    try:
+        if jobid is not None:
+            jarvice_api.tail(jobid, lines)
+        elif jobname is not None:
+            jarvice_api.tail(jobname, lines)
+    except Exception as e:
+        print(e)
+
+@jarvice_cli.command()
+def output(
+    jobid: Annotated[Optional[int], typer.Option("-j", "--jobid",help="ID of the job [required or --jobname required]")] = None,
+    jobname: Annotated[Optional[str], typer.Option("-n", "--jobname",help="Name of the job [required or --jobid required]")] = None,
+    lines: Annotated[
+        Optional[int], typer.Option("-l", "--lines",  help="Number of lines to display")
+    ] = 0
+):
+    """
+    See the output of a job that has ended
+    """
+    validateJobidJobname(jobid, jobname)    
+    try:
+        if jobid is not None:
+            jarvice_api.output(jobid, lines)
+        elif jobname is not None:
+            jarvice_api.output(jobname, lines)
+    except Exception as e:
+        print(e)
+
+@jarvice_cli.command()
+def connect(
+    jobid: Annotated[Optional[int], typer.Option("-j", "--jobid",help="ID of the job [required or --jobname required]")] = None,
+    jobname: Annotated[Optional[str], typer.Option("-n", "--jobname",help="Name of the job [required or --jobid required]")] = None,
+):
+    """
+    Get connection details (address, password)
+    """
+    validateJobidJobname(jobid, jobname)
+    try:
+        if jobid is not None:
+            jarvice_api.connect(jobid)
+        elif jobname is not None:
+            jarvice_api.connect(jobname)
+    except Exception as e:
+        print(e)
+
+@jarvice_cli.command()
+def shutdown(
+    jobid: Annotated[Optional[int], typer.Option("-j", "--jobid",help="ID of the job [required or --jobname required]")] = None,
+    jobname: Annotated[Optional[str], typer.Option("-n", "--jobname",help="Name of the job [required or --jobid required]")] = None,
+):
+    """
+    Cleanly shutdown a job (with shutdown signal)
+    """
+    validateJobidJobname(jobid, jobname)
+    try:
+        if jobid is not None:
+            jarvice_api.shutdown(jobid)
+        elif jobname is not None:
+            jarvice_api.shutdown(jobname)
+    except Exception as e:
+        print(e)
+
+@jarvice_cli.command()
+def terminate(
+    jobid: Annotated[Optional[int], typer.Option("-j", "--jobid",help="ID of the job [required or --jobname required]")] = None,
+    jobname: Annotated[Optional[str], typer.Option("-n", "--jobname",help="Name of the job [required or --jobid required]")] = None,
+):
+    """
+    Force termination of a job (like kill -9)
+    """
+    validateJobidJobname(jobid, jobname)
+    try:
+        if jobid is not None:
+            jarvice_api.terminate(jobid)
+        elif jobname is not None:
+            jarvice_api.terminate(jobname)
+    except Exception as e:
+        print(e)
+
+@jarvice_cli.command()
+def info(
+    jobid: Annotated[Optional[int], typer.Option("-j", "--jobid",help="ID of the job [required or --jobname required]")] = None,
+    jobname: Annotated[Optional[str], typer.Option("-n", "--jobname",help="Name of the job [required or --jobid required]")] = None,
+):
+    """
+    Get the stats on your job
+    """
+    validateJobidJobname(jobid, jobname)
+    try:
+        if jobid is not None:
+            jarvice_api.info(jobid)
+        elif jobname is not None:
+            jarvice_api.info(jobname)
+    except Exception as e:
+        print(e)
+
+@jarvice_cli.command()
+def status(
+    jobid: Annotated[Optional[int], typer.Option("-j", "--jobid",help="ID of the job [required or --jobname required]")] = None,
+    jobname: Annotated[Optional[str], typer.Option("-n", "--jobname",help="Name of the job [required or --jobid required]")] = None,
+):
+    """
+    Get status of a job
+    """
+    validateJobidJobname(jobid, jobname)
+    try:
+        if jobid is not None:
+            jarvice_api.status(jobid)
+        elif jobname is not None:
+            jarvice_api.status(jobname)
+    except Exception as e:
+        print(e)
+
+@jarvice_cli.command(deprecated=True)
+def action(
+    action: Annotated[str, typer.Argument(help="action name, given by info")],
+    jobid: Annotated[Optional[int], typer.Option("-j", "--jobid",help="ID of the job [required or --jobname required]")] = None,
+    jobname: Annotated[Optional[str], typer.Option("-n", "--jobname",help="Name of the job [required or --jobid required]")] = None,
+):
+    """
+    Perform a configured action on your job
+    """
+    validateJobidJobname(jobid, jobname)
+    try:
+        if jobid is not None:
+            jarvice_api.action(action, jobid)
+        elif jobname is not None:
+            jarvice_api.action(action, jobname)
+    except Exception as e:
+        print(e)
+
+@jarvice_cli.command()
+def jobs():
+    """
+    Get a list of currently running jobs
+    """
+    try:
+        jarvice_api.jobs()
+    except Exception as e:
+        print(e)
+
+@jarvice_cli.command()
+def shutdown_all():
+    """
+    Cleanly shutdown all currently running jobs
+    """
+    try:
+        jarvice_api.shutdown_all()
+    except Exception as e:
+        print(e)
+
+@jarvice_cli.command()
+def terminate_all():
+    """
+    Force termination of all jobs (like kill -9)
+    """
+    try:
+        jarvice_api.terminate_all()
+    except Exception as e:
+        print(e)
+
+@jarvice_cli.command()
+def wait_for(
+    jobid: Annotated[Optional[int], typer.Option("-j", "--jobid",help="ID of the job [required or --jobname required]")] = None,
+    jobname: Annotated[Optional[str], typer.Option("-n", "--jobname",help="Name of the job [required or --jobid required]")] = None,
+):
+    """
+    Wait for a job to end
+    """
+    validateJobidJobname(jobid, jobname)
+    try:        
+        if jobid is not None:
+            jarvice_api.wait_for(jobid)
+        elif jobname is not None:
+            jarvice_api.wait_for(jobname)
+    except Exception as e:
+        print(e)
+
+
+## Data Management ##
+
+@jarvice_cli.command()
+def download(
+    source: Annotated[str, typer.Argument(help="File/directory to download")],
+    destination: Annotated[str, typer.Argument(help="File/directory destination")],
+    storage : Annotated[str, typer.Argument(help="vault name")],
+):
+    """
+    Download file or directory 
+    """
+    try:        
+        jarvice_api.download(source, destination, storage)
+    except Exception as e:
+        print(e)
+
+@jarvice_cli.command()
+def upload(
+    source: Annotated[str, typer.Argument(help="File/directory to upload")],
+    destination: Annotated[str, typer.Argument(help="File/directory destination")],
+    storage : Annotated[str, typer.Argument(help="vault name")],
+):
+    """
+    Upload file or directory 
+    """
+    try:        
+        jarvice_api.upload(source, destination, storage)
+    except Exception as e:
+        print(e)
+
+@jarvice_cli.command()
+def ls(
+    storage : Annotated[str, typer.Argument(help="vault name")],
+    remote_dir: Annotated[
+        Optional[str], typer.Option("-d", help="Remote directory")
+    ] = None,
+):
+    """
+    List files
+    """
+    try:        
+        jarvice_api.ls(storage, remote_dir)
+    except Exception as e:
+        print(e)
+
+## Querying JARVICE Options ##
+
+@jarvice_cli.command()
+def apps():
+    """
+    List apps and gives a schema describing outputs
+    """
+    try:        
+        jarvice_api.apps()
+    except Exception as e:
+        print(e)
+
+@jarvice_cli.command()
+def machines():
+    """
+    List all instances
+    """
+    try:        
+        jarvice_api.machines()
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__":
+    jarvice_cli()
---
+++ jarvice_cli/jarviceapi.py
@@ -0,0 +1,206 @@
+from collections import OrderedDict
+from typing import Optional, Tuple, Union
+import json
+import logging
+import jarviceapi_client
+from pathlib import Path
+
+class jarviceapi:
+    _username : str
+    _apikey : str
+    _uconfiguration : str
+
+    def __init__(self, username : str, apikey : str, url : str):
+        self._username = username
+        self._apikey = apikey
+        self._configuration = jarviceapi_client.Configuration(
+            host = url
+        )
+
+    def submit(self, job_jsonfile : Path):
+        """
+        Submits a job to Jarvice XE
+        
+        Args:
+            job_jsonfile (Path) : json file containing job launch definition
+
+        Raises:
+            Exception: Raises an exception.
+        """
+        raise Exception("submit is not implemented")
+
+    def summary(self):
+        """
+        List a summary of your jobs
+        
+        Raises:
+            Exception: Raises an exception.
+        """
+        retval = OrderedDict()
+
+        raise Exception("summary is not implemented")
+        return retval
+    
+    def tail(self, job : Union[int, str], lines):
+        """
+        List a summary of your jobs
+        
+        Raises:
+            Exception: Raises an exception.
+        """
+        retval : str
+        raise Exception("tail is not implemented")
+        return retval
+    
+    def output(self, job : Union[int, str], lines):
+        retval : str
+        raise Exception("output is not implemented")
+        return retval
+    
+    def connect(self, job : Union[int, str]):
+        """
+        Get connection details (address, password)
+        Raises:
+            Exception: Raises an exception.
+        """
+        address : str
+        password : str
+        raise Exception("connect is not implemented")
+        return address, password
+    
+    def shutdown(self, job : Union[int, str]):
+        """
+        Cleanly shutdown a job (with shutdown signal)
+
+        Raises:
+            Exception: Raises an exception.
+        """
+        raise Exception("shutdown is not implemented")
+    
+    def terminate(self, job : Union[int, str]):
+        """
+        Force termination of a job (like kill -9)
+
+        Raises:
+            Exception: Raises an exception.
+        """
+        raise Exception("terminate is not implemented")
+
+    def info(self, job : Union[int, str]):
+        """
+        Get the stats on your job
+
+        Raises:
+            Exception: Raises an exception.
+        """
+        raise Exception("info is not implemented")
+
+    def status(self, job : Union[int, str]):
+        """
+        Get status of a job
+
+        Raises:
+            Exception: Raises an exception.
+        """
+        
+        raise Exception("status is not implemented")
+
+    def action(self, action : str, job : Union[int, str]):
+        """
+        Perform a configured action on your job
+
+        Raises:
+            Exception: Raises an exception.
+        """
+        raise Exception("action is not implemented")
+
+
+    def jobs(self):
+        """
+        Get a list of currently running jobs
+
+        Raises:
+            Exception: Raises an exception.
+        """
+        with jarviceapi_client.ApiClient(self._configuration) as api_client:
+            api_instance = jarviceapi_client.StatusAndInformationApi(api_client)
+            try:
+                api_response = api_instance.jobs_get(self._apikey, self._username)
+                print(api_response)
+            except Exception as e:
+                print("Exception when calling jobs: %s\n" % e)
+            
+
+    def shutdown_all(self):
+        """
+        Cleanly shutdown all currently running jobs
+
+        Raises:
+            Exception: Raises an exception.
+        """
+        raise Exception("shutdown_all is not implemented")
+    
+    
+    def terminate_all(self):
+        """
+        Force termination of all jobs (like kill -9)
+
+        Raises:
+            Exception: Raises an exception.
+        """
+        raise Exception("terminate_all is not implemented")
+
+    def wait_for(self, job : Union[int, str]):
+        """
+        Wait for a job to end
+        
+        Raises:
+            Exception: Raises an exception.
+        """
+        raise Exception("wait_for is not implemented")
+
+    def download(self, source: str, destination: str, storage : str):
+        """
+        Download file or directory 
+        
+        Raises:
+            Exception: Raises an exception.
+        """
+        raise Exception("download is not implemented")
+
+    def upload(self, source: str, destination: str, storage : str):
+        """
+        Upload file or directory
+        
+        Raises:
+            Exception: Raises an exception.
+        """
+        raise Exception("download is not implemented")
+    
+    def ls(self, storage : str, remote_dir: Optional[str] = None):
+        """
+        List files
+
+        Raises:
+            Exception: Raises an exception.
+        """
+        raise Exception("ls is not implemented")
+
+    def apps(self):
+        """
+        List apps and gives a schema describing outputs
+
+        Raises:
+            Exception: Raises an exception.
+        """
+        raise Exception("apps is not implemented")
+
+    def machines(self):
+        """
+        List all instances
+
+        Raises:
+            Exception: Raises an exception.
+        """
+        raise Exception("machines is not implemented")
+
